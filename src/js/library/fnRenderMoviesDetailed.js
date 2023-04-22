8f56beee5bc83f7547e39a22b74483c73c8f9242
Hordiyenko Tetyana <hordit@me.com>
2023-04-23 00:07:32 +0200

    update

--- src/js/library/fnRenderMoviesDetailed.js
+++ src/js/library/fnRenderMoviesDetailed.js
@@ -1,42 +1,51 @@
+import genresData from '../../genres.json';
 import { refs } from '../refs';
-import MovieApi from '../apiService';
-import noPoster from '../../img/no-poster.jpg';
-import '../../sass/_gallery.scss';
+const genres = genresData.genres;
 
-export async function renderMoviesDetailed(data) {
+async function renderMoviesDetailed(data) {
   if (!data) {
     return;
   }
-  const movieApi = new MovieApi();
-  const genresArray = await movieApi.getGenres();
-
   refs.gallery.innerHTML = data
-  .map(movie => {
-  const imageSrc = movie.poster_path
-  ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-  : noPoster;
-  const genres = genersForFilmCard(movie.genre_ids, genresArray);
-  const year = movie.release_date ? movie.release_date.split('-')[0] : 'n/a';
-  return `
-  <li class='gallery__item' data-id="${movie.id}">
-   <img class='gallery__item-image' alt="${movie.title}" src="${imageSrc}">
-   <div class='gallery__item-description'> 
-      <h3 class='gallery__item-description-title'>
-      ${movie.title}
-      </h3>
-      <p class='gallery__item-description-genres'>
-       ${genres.slice(0, 2).concat('...').join(', ')}
-       | ${year}
-      </p> 
-    </div>
-   </li>
-  `;
-  })
-  .join('');
+    .map(movie => {
+      const imageSrc = movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : '';
+      const genres = genersForFilmCard(makeGenresArray(movie));
+
+      const year = movie.release_date
+        ? movie.release_date.split('-')[0]
+        : 'n/a';
+      return `
+  <li  class='gallery__item' data-id="${movie.id}">
+      <img class='gallery__item-image' alt="${movie.title}" src="${imageSrc}">
+    <div class='gallery__item-description'>
+    <h3 class='gallery__item-description-title' >${movie.title}</h3>
+    <p class='gallery__item-description-genres' >${genres} | ${year}</p></div>
+  </li>
+`;
+    })
+    .join('');
 }
 
-// Генерируем жанры по id-шнику
-function genersForFilmCard(arrayOfGenersID, genresArray) {
-const arrayOfGenres = arrayOfGenersID.map(id => genresArray.find(genre => genre.id === id).name);
-return arrayOfGenres;
+// Массив для функции genersForFilmCard
+function makeGenresArray(movie) {
+  let genresIdsArray = [];
+  movie.genres.forEach(genre => {
+    genresIdsArray.push(genre.id);
+  });
+  return genresIdsArray;
 }
+// Обработка ID жанров и вывод их наименований для renderMoviesDetailed
+function genersForFilmCard(genreIds) {
+  const arrayOfGenres = genreIds.map(id => {
+    return genres.find(genre => genre.id === id).name;
+  });
+  const result =
+    arrayOfGenres.length > 2
+      ? arrayOfGenres.splice(0, 2).concat('Other').join(', ')
+      : arrayOfGenres.join(', ');
+  return result;
+}
+
+export { makeGenresArray, genersForFilmCard, renderMoviesDetailed };
